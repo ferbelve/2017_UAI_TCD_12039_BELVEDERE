fd1e46330e53a864d76f847f5acb46cca8c1c764
ferbelve <fgbelvedere@gmail.com>
2017-04-23 06:39:16 -0300

    Prueba

---
+++ SISFRA/Persistencia/conexion.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.SqlClient
+Public Class conexion
+    Protected cnn As New SqlConnection
+
+    Protected Function conectado()
+        Try
+            cnn = New SqlConnection(My.Settings.Conexion)
+            cnn.Open()
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+    Protected Function desconectado()
+        Try
+            If cnn.State = ConnectionState.Open Then
+                cnn.Close()
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+End Class
---
+++ SISFRA/Presentacion/frmsucursales.vb
@@ -0,0 +1,27 @@
+﻿Imports Persistencia
+Imports Negocio
+
+
+
+Public Class frmsucursales
+    Private dt As New DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mostrar()
+    End Sub
+
+    Private Sub mostrar()
+        Try
+            Dim func As New Sucursales
+            dt = func.mostrar
+
+            If dt.Rows.Count <> 0 Then
+                DataGridView1.DataSource = dt
+            Else
+                DataGridView1.DataSource = Nothing
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+End Class
